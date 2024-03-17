d8f39bce669c50d94ac91fc7e27ed1a77076d5ef
Matthew McKinnon <mmckinnon@comprofix.com>
2024-03-17 15:06:49 +1000

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,10 @@ elif [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
     ln -s `pwd`/bash/bash_aliases ~/.bash_aliases
 fi
 
+# Setup git
+rm -fr ~/.gitconfig
+ln -s `pwd`/git/gitconfig ~/.gitconfig
+
 # Setup nano
 echo "Setup nano"
 rm -fr ~/.nano
@@ -34,6 +38,35 @@ echo "Setup mutt"
 echo "Make sure you generate an oauth2 file - ./mutt_oauth2.py oauth2 --verbose --authorize"
 rm -fr ~/.mutt
 ln -s `pwd`/mutt ~/.mutt
+
+# Setup lxterminal
+echo "Setup lxterminal"
+rm -fr ~/.config/lxterminal
+ln -s `pwd`/config/lxterminal ~/.config/lxterminal
+
+# Setup AwesomeWM
+echo "Setup AwesomeWM"
+rm -fr ~/.xinitrc
+ln -s `pwd`/xinitrc ~/.xinitrc
+ln -s `pwd`/config/awesome ~/.config/awesome
+
+# Setup mpd
+rm -fr ~/.config/mpd
+ln -s `pwd`/config/mpd ~/.config/mpd
+
+# Setup picom
+rm -fr ~/.config/picom
+ln -s `pwd`/config/picom ~/.config/picom
+
+# Setup qutebrowser
+rm -fr ~/.config/qutebrowser
+ln -s `pwd`/config/qutebrowser ~/.config/qutebrowser
+
+# Set Newsboat
+rm -fr ~/.config/newsboat
+ln -s `pwd`/config/newsboat ~/.config/newsboat
+
+
 
 
 

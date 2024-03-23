02406cbb1b5c9adf4a2dc96a4e7bdfafea97dc87
Matthew McKinnon <mmckinnon@comprofix.com>
2024-03-23 14:16:54 +1000

    fix: Mutt Signature. Added startup items for Awesome

--- config/awesome/autostart.sh
+++ config/awesome/autostart.sh
@@ -8,6 +8,9 @@ function run {
 }
 
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run xscreensaver
 run nm-applet
 run parcellite
 #run picom --config $HOME/.config/picom/picom.conf
+run bitwarden-desktop
+run remmina -i
